e53368aa96cac2b0738d49dcd961824a0fa6282b
lrbuller <lrbuller@mtu.edu>
2024-04-29 10:28:18 -0400

    added download function and profile argument

--- src/statmagic_backend/dev/aws_cli.py
+++ src/statmagic_backend/dev/aws_cli.py
@@ -8,9 +8,35 @@ import re
 import logging
 logger = logging.getLogger("statmagic_backend")
 
-def ls(endpoint, bucket, path, pattern, recursive=False):
-    s3_session = boto3.Session(profile_name="default")
-    s3_client = s3_session.client("s3", endpoint_url=endpoint)
+def ls(profile, endpoint, bucket, path, pattern, recursive=False):
+    """
+        Search for files in S3 Bucket
+
+        Parameters
+        ----------
+        profile: str
+            Name of profile in AWS credentials file where access keys are saved
+        endpoint : str
+            Endpoint URL of s3 bucket
+        bucket : str
+            Bucket to search
+        path : str, optional
+            Path within AWS bucket to limit search to
+        pattern : str, optional
+            Search pattern
+        recursive : bool, optional
+            Whether to recursively search subfolders
+
+        Returns
+        -------
+        files: list
+            List of object names
+        """
+    s3_session = boto3.Session(profile_name=profile)
+    if endpoint:
+        s3_client = s3_session.client("s3", endpoint_url=endpoint)
+    else:
+        s3_client = s3_session.client("s3")
     if path:
         contents = s3_client.list_objects_v2(Bucket=bucket, Prefix=path)['Contents']
     else:
@@ -33,12 +59,14 @@ def ls(endpoint, bucket, path, pattern, recursive=False):
     return files
 
 
-def upload(endpoint, filename, bucket, object_name=None, extra_args=None, callback=None):
+def upload(profile, endpoint, filename, bucket, object_name=None, extra_args=None, callback=None):
     """
     Upload a file to an S3 Bucket
 
     Parameters
     ----------
+    profile: str
+        Name of profile in AWS credentials file where access keys are saved
     endpoint : str
         Endpoint URL of s3 bucket
     filename : str
@@ -73,10 +101,64 @@ def upload(endpoint, filename, bucket, object_name=None, extra_args=None, callba
         object_name = Path(filename).stem
 
     # Upload the file
-    s3_session = boto3.Session(profile_name="default")
-    s3_client = s3_session.client("s3", endpoint_url=endpoint)
+    s3_session = boto3.Session(profile_name=profile)
+    if endpoint:
+        s3_client = s3_session.client("s3", endpoint_url=endpoint)
+    else:
+        s3_client = s3_session.client("s3")
     try:
         response = s3_client.upload_file(filename, bucket, object_name)
+    except ClientError as e:
+        # logging.error(e)
+        return False
+    return True
+
+
+def download(profile, endpoint, bucket, object_name, filename, extra_args=None, callback=None):
+    """
+    Download a file from an S3 Bucket
+
+    Parameters
+    ----------
+    profile: str
+        Name of profile in AWS credentials file where access keys are saved
+    endpoint : str
+        Endpoint URL of s3 bucket
+    bucket : str
+        Bucket to download from
+    object_name : str
+        S3 object name.
+    filename : str
+        File to save to.
+    extra_args : dict, optional
+        Additional keyword arguments to :meth:`boto3.Client.download_file`
+    callback : callable, optional
+        Instance of a class whose ``__call__`` method displays upload progress
+
+    Returns
+    -------
+    success : bool
+        ``True`` if upload succeeded, ``False`` otherwise.
+    """
+    # default values
+    if extra_args is None:
+        extra_args = {}
+    if callback is None:
+        class NullCallback(object):
+            def __init__(self):
+                pass
+            def __call__(self):
+                pass
+        callback = NullCallback()
+
+    # Download the file
+    s3_session = boto3.Session(profile_name=profile)
+    if endpoint == '':
+        s3_client = s3_session.client("s3")
+    else:
+        s3_client = s3_session.client("s3", endpoint_url=endpoint)
+    try:
+        response = s3_client.download_file(bucket, object_name, filename)
     except ClientError as e:
         # logging.error(e)
         return False
